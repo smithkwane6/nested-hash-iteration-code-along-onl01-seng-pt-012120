04c7cdf79b1602258c7cde31e93ff3ad3926a9ff
Kwane Smith <smithkwane6@gmail.com>
2020-02-02 22:54:28 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -13,13 +13,15 @@ require 'pry'
   #     favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
   #   }
   # }
-
-def remove_strawberry(contacts)
-  contacts.each do |person, contact_details_hash|
-    contact_details_hash.each do |attribute, data|
+  def remove_strawberry(contacts)
+    # Assign Freddy Mercury's hash to a variable
+    freddy = contacts["Freddy Mercury"]
+  
+    # Iterate over only Freddy Mercury's data
+    freddy.each do |attribute, value|
       if attribute == :favorite_icecream_flavors
-        data.delete_if { |ice_cream| ice_cream == "strawberry"}
+        value.delete_if{ |ice_cream| ice_cream == "strawberry" }
       end
     end
-  end
-end
+    contacts
+  end 
